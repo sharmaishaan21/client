0550a184800664f49b3e3731e56d9c31e2449b55
sharmaishaan21 <78147592+sharmaishaan21@users.noreply.github.com>
2022-06-17 23:00:35 +0530

    save

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const usersUrl = 'http://localhost:3003/users';
-const usersUrl = 'https://temp-project-007.herokuapp.com/';
+const usersUrl = 'https://temp-project-007.herokuapp.com';
 
 export const getUsers = async (id) => {
     id = id || '';
